bed169cca76f0ca85d52a26cedc2ac699013598a
Melanieynov <marmande.melanie@gmail.com>
2024-04-02 16:45:04 +0200

    fetching form data

--- admin_area/view_products.php
+++ admin_area/view_products.php
@@ -46,7 +46,7 @@
             
             ?></td>
             <td><?php   echo $status;?></td>
-            <td><a href='index.php?edit_products' class='text-light'><i class='fa-solid fa-pen-to-square'></i></a></td>
+            <td><a href='index.php?edit_products=<?php echo $product_id?>' class='text-light'><i class='fa-solid fa-pen-to-square'></i></a></td>
             <td><a href='' class='text-light'><i class='fa-solid fa-trash'></i></a></td>
             </tr>
             <?php
