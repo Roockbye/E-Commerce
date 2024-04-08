d6a13e99862c8551042a753666dc7f3c6266336e
Gaëlle Lanic <gaelle.lanic@ynov.com>
2024-04-08 11:46:38 +0200

    step 3

--- common_function.php
+++ common_function.php
@@ -22,12 +22,12 @@ function getproducts()
                 echo "
         <div class='col-md-4 mb-2'>
         <div class='card'>
-            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='...'>
+            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
-                <a href='#' class='btn btn-secondary'>Voir plus</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
         </div>
@@ -57,12 +57,12 @@ function get_all_products(){
                 echo "
         <div class='col-md-4 mb-2'>
         <div class='card'>
-            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='...'>
+            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
-                <a href='#' class='btn btn-secondary'>Voir plus</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
         </div>
@@ -117,12 +117,12 @@ function get_unique_categories()
             echo "
         <div class='col-md-4 mb-2'>
         <div class='card'>
-            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='...'>
+            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
-                <a href='#' class='btn btn-secondary'>Voir plus</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
         </div>
@@ -176,12 +176,12 @@ function get_unique_brands()
             echo "
         <div class='col-md-4 mb-2'>
         <div class='card'>
-            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='...'>
+            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
-                <a href='#' class='btn btn-secondary'>Voir plus</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
         </div>
@@ -215,12 +215,12 @@ function search_product()
         echo "
         <div class='col-md-4 mb-2'>
         <div class='card'>
-            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='...'>
+            <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
-                <a href='#' class='btn btn-secondary'>Voir plus</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
         </div>
@@ -229,64 +229,71 @@ function search_product()
     }
 }
 
-// function view_details()
-// {
-//     global $con;
-//     if (isset($_GET['product_id'])) {
-//         if (!isset($_GET['category'])) {
-//             if (!isset($_GET['brand'])) {
-//                 $product_id = $_GET['product_id'];
-//                 $select_query = "Select * from `products` where product_id=$product_id";
-//                 $result_query = mysqli_query($con, $select_query);
-//                 while ($row = mysqli_fetch_assoc($result_query)) {
-//                     $product_id = $row['product_id'];
-//                     $product_title = $row['product_title'];
-//                     $product_description = $row['product_description'];
-//                     $product_image1 = $row['product_image1'];
-//                     $product_image2 = $row['product_image2'];
-//                     $product_image3 = $row['product_image3'];
-//                     $product_price = $row['product_price'];
-//                     $category_id = $row['category_id'];
-//                     $brand_id = $row['brand_id'];
-//                     echo "<div class='col-md-4 mb-2'>
-//                 <div class='card'>
-//                 <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
-//                 <div class='card-body'>
-//                 <h5 class='card-title'>$product_title</h5>
-//                 <p class='card-text'>$product_description</p>
-//                 <p class='card-text'>Price: $product_price/-</p>
-//                 <a href='index.php?add_to_cart=$product_id' class='btn btn-info'>Add to cart</a>
-//                 <a href='index.php' class='btn btn-secondary'>Go home</a>
-//                 </div>
-//                 </div>
-//                 </div>
-                
-//                 <div class='col-md-8'>
-//                 <div class='row'>
-//                 <div class='col-md-12'>
-//                 <h4 class='text-center text-info mb-5'>Related products</h4>
-//                 </div>
-//                 <div class='col-md-6'>
-//                 <img src='./admin_area/product_images/$product_image2' class='card-img-top' alt='$product_title'>
-//                 </div>
-//                 <div class='col-md-6'>
-//                 <img src='./admin_area/product_images/$product_image3' class='card-img-top' alt='$product_title'>
-//                 </div>
-//                 </div>
-//                 </div>";
-//                 }
-//             }
-//         }
-//     }
-// }
-// function getIPAddress()
-// {
-//     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-//         $ip = $_SERVER['HTTP_CLIENT_IP'];
-//     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-//         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-//     } else {
-//         $ip = $_SERVER['REMOTE_ADDR'];
-//     }
-//     return $ip;
-// }
+function view_details()
+{
+    global $con;
+
+    if (isset($_GET['product_id'])) {
+        if (!isset($_GET['category'])) {
+            if (!isset($_GET['brand'])) {
+                $product_id = $_GET['product_id'];
+                $select_query = "SELECT * FROM products WHERE product_id=$product_id";
+                $result_query = mysqli_query($con, $select_query);
+
+                while ($row = mysqli_fetch_assoc($result_query)) {
+                    $product_id = $row['product_id'];
+                    $product_title = $row['product_title'];
+                    $product_description = $row['product_description'];
+                    $product_image1 = $row['product_image1'];
+                    $product_image2 = $row['product_image2'];
+                    $product_image3 = $row['product_image3'];
+                    $product_price = $row['product_price'];
+                    $category_id = $row['category_id'];
+                    $brand_id = $row['brand_id'];
+                    echo "
+                    <div class='col-md-4 mb-2'>
+                    <div class='card'>
+                        <img src='./admin_area/product_images/$product_image1' class='card-img-top' alt='$product_title'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>$product_title</h5>
+                            <p class='card-text'>$product_description</p>
+                            <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
+                            <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
+                        </div>
+                    </div>
+                    </div>
+                       
+                    <div class='col-md-8'>
+                        <!-- images -->
+                        <div class='row'>
+                            <div class='col-md-12'>
+                                <h4 class='text-center'>Images associées</h4>
+                            </div>
+                            <div class='col-mb-6'>
+                                <img src='./admin_area/product_images/$product_image2' class='card-img-top' alt='$product_title'>
+                            </div>
+                            <div class='col-mb-6'>
+                                <img src='./admin_area/product_images/$product_image3' class='card-img-top' alt='$product_title'>
+                            </div>
+                        </div>
+                    </div>";
+                }
+            }
+        }
+    }
+}
+
+function getIPAddress()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } 
+    
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } 
+    else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
