cd79ee96500dc7a2cea6965aa4bec70ed0c7343e
Gaëlle Lanic <gaelle.lanic@ynov.com>
2024-04-08 11:46:52 +0200

    step 3

--- index.php
+++ index.php
@@ -90,6 +90,8 @@ include('./functions/common_function.php');
     getproducts();
     get_unique_categories();
     get_unique_brands();
+    $ip = getIPAddress();
+    // echo 'User Real IP Address - '.$ip;
     ?>
                 </div>
             </div>
