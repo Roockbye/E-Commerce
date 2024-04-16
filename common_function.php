f3bc6559535d4966092ce6acdde918c4c7bb9f96
Gaëlle Lanic <gaelle.lanic@ynov.com>
2024-04-16 14:58:01 +0200

    update

--- common_function.php
+++ common_function.php
@@ -26,7 +26,8 @@ function getproducts()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
+                <p class='card-text'>Prix : $product_price €</p>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-outline-success'>Ajouter au panier</a>
                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
@@ -61,7 +62,8 @@ function get_all_products(){
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
+                <p class='card-text'>Prix : $product_price €</p>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-outline-success'>Ajouter au panier</a>
                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
@@ -121,7 +123,8 @@ function get_unique_categories()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
+                <p class='card-text'>Prix : $product_price €</p>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-outline-success'>Ajouter au panier</a>
                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
@@ -180,7 +183,8 @@ function get_unique_brands()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
+                <p class='card-text'>Prix : $product_price €</p>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-outline-success'>Ajouter au panier</a>
                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
@@ -219,7 +223,8 @@ function search_product()
             <div class='card-body'>
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
-                <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
+                <p class='card-text'>Prix : $product_price €</p>
+                <a href='index.php?add_to_cart=$product_id' class='btn btn-outline-success'>Ajouter au panier</a>
                 <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
             </div>
         </div>
@@ -257,12 +262,12 @@ function view_details()
                         <div class='card-body'>
                             <h5 class='card-title'>$product_title</h5>
                             <p class='card-text'>$product_description</p>
-                            <a href='#' class='btn btn-outline-success'>Ajouter au panier</a>
-                            <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>Voir plus</a>
+                            <p class='card-text'>Prix : $product_price €</p>
+                            <a href='index.php?add_to_cart=$product_id' class='btn btn-outline-success'>Ajouter au panier</a>
+                            <a href='index.php' class='btn btn-secondary'>Retour à l'accueil</a>
                         </div>
                     </div>
-                    </div>
-                       
+                    </div>                       
                     <div class='col-md-8'>
                         <!-- images -->
                         <div class='row'>
@@ -296,4 +301,64 @@ function getIPAddress()
         $ip = $_SERVER['REMOTE_ADDR'];
     }
     return $ip;
+}
+
+function cart(){
+    if(isset($_GET['add_to_cart'])){
+        global $con;
+        $get_ip_add = getIPAddress();
+        $get_product_id=$_GET['add_to_cart'];        
+        $select_query="SELECT * FROM cart_details WHERE ip_address='$get_ip_add' AND product_id=$get_product_id";
+        $result_query = mysqli_query($con, $select_query);
+
+        $num_of_rows = mysqli_num_rows($result_query);
+        if ($num_of_rows > 0) {
+            echo "<script>alert('Cet article est déjà présent dans le panier')</script>";
+            echo "<script>windows.open('index.php','_self')</script>";
+        } else {
+            $insert_query="INSERT INTO cart_details (product_id, ip_address, quantity) VALUES ($get_product_id, '$get_ip_add', 0)";
+            $result_query = mysqli_query($con, $insert_query);
+            echo "<script>alert('Produit ajouté au panier')</script>";
+            echo "<script>windows.open('index.php','_self')</script>";
+        }
+    }
+}
+
+//get cart item numbers
+function cart_item(){
+    if(isset($_GET['add_to_cart'])){
+        global $con;
+        $get_ip_add = getIPAddress();
+        $select_query="SELECT * FROM cart_details WHERE ip_address='$get_ip_add'";
+        $result_query = mysqli_query($con, $select_query);
+        $count_cart_items = mysqli_num_rows($result_query);
+        } else {
+            global $con;
+            $get_ip_add = getIPAddress();  
+            $select_query="SELECT * FROM cart_details WHERE ip_address='$get_ip_add'";
+            $result_query = mysqli_query($con, $select_query);
+            $count_cart_items = mysqli_num_rows($result_query);
+        }
+    echo $count_cart_items;
+}
+
+function total_cart_price(){
+    global $con;
+    $get_ip_add = getIPAddress();
+    $total_price = 0;
+    $cart_query = "SELECT * FROM cart_details WHERE ip_address='$get_ip_add'";
+    $result = mysqli_query($con, $cart_query);
+    
+    while($row = mysqli_fetch_array($result)){
+        $product_id = $row['product_id'];
+        $select_products = "SELECT * FROM products WHERE product_id='$product_id'";
+        $result_products = mysqli_query($con, $select_products);
+
+        while($row_product_price = mysqli_fetch_array($result_products)){
+            $product_price = array($row_product_price['product_price']);
+            $product_values = array_sum($product_price);
+            $total_price += $product_values;
+        }
+    }
+    echo $total_price;
 }
