f6864c51cadea4fa59893db06fe2550293cb27f6
Melanieynov <marmande.melanie@gmail.com>
2024-05-03 10:52:46 +0200

    front login/register admin

--- admin_area/admin_login.php
+++ admin_area/admin_login.php
@@ -33,34 +33,38 @@ if (isset($_POST['admin_login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../static/css/index.css">
+    <link rel="stylesheet" href="../static/css/admin_login.css">
 </head>
 <body>
-    <div class="container-fluid m-3">
-        <h2 class="text-center mb-5">Admin Login</h2>
-    <div class="row d-flex justify-content-center">
-        <div class="col-lg-6 col-xl-5">
-            <img src="../static/images/reg.png" alt="Admin Registration" class="img-fluid">
-        </div>
-        <div class="col-lg-6 col-xl-4">
-            <form action="" method="post">
-                <div class="form-outline mb-4">
-                    <label for="admin_username" class="form-label">Username</label>
-                    <input type="text" id="admin_username" name="admin_username" placeholder="Enter your username" required="required" class="form-control">
-                </div>
-                <div class="form-outline mb-4">
-                    <label for="admin_password" class="form-label">Password</label>
-                    <input type="password" id="admin_password" name="admin_password" placeholder="Enter your password" required="required" class="form-control">
-                </div>
-                <div>
-                    <input type="submit" class="bg-info py-2 px-3 border-0" name="admin_login" value="Login">
-                    <p class="small fw-bold mt-2 pt-1">You don't have an account ?<a href="admin_registration.php">Register</a></p>
-                </div>
-            </form>
-        </div>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-3 col-md-2"></div>
+    <div class="col-lg-6 col-md-8 login-box">
+      <div class="col-lg-12 login-key">
+        <i class="fa fa-key" aria-hidden="true"></i>
+      </div>
+      <div class="col-lg-12 login-title">
+        ADMIN LOGIN
+      </div>
+
+      <div class="col-lg-12 login-form">
+        <form method="POST" action="">
+          <div class="form-group">
+            <label for="admin_username" class="form-control-label">Username</label>
+            <input type="text" id="admin_username" name="admin_username" required="required" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="admin_password" class="form-control-label">Password</label>
+            <input type="password" id="admin_password" name="admin_password" required="required" class="form-control">
+          </div>
+          <div class="col-lg-12 login-btm login-button">
+            <input type="submit" class="btn btn-outline-primary" name="admin_login" value="Login">
+            <p class="register-link">You don't have an account ? <a href="admin_registration.php">Register</a></p>
+          </div>
+        </form>
+      </div>
     </div>
-    
+  </div>
+</div>
 </body>
 </html>
